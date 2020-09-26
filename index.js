ad827bba6ca4055d3e412efdebf120f37ea807b5
João Teixeira <jpteixeira1308@gmail.com>
2020-09-26 17:10:59 -0300

    Alterações 26-09-2020

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const session = require("express-session");
 const flash = require("express-flash");
 // Importando body-parser
 const bodyParser = require("body-parser");
+// Importando cookie parser, manda informações para o front end
+const cookieParser = require("cookie-parser");
+
 
 // Setando view engine
 app.set("view engine", "ejs");
@@ -17,12 +20,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
+// Ativando cookie Parser passando qualquer coisa como "senha"
+app.use(cookieParser("kdjasbdjkhabskjdb"))
+
 // Configuração express session
 app.use(session({
     secret: 'keyboard cat',
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: true }
+    cookie: { maxAge: 60000 }
 }));
 
 // Configuração express flash
@@ -30,7 +36,20 @@ app.use(flash());
 
 // Rota principal
 app.get("/", (req, res) => {
-    res.render("index");
+    // Acessando flash sessions da outra rota
+    var emailError = req.flash("emailError");
+    var nomeError = req.flash("nomeError");
+    var pontosError = req.flash("pontosError");
+    var email = req.flash("email");
+
+    // Operação ternária, substitui if
+    emailError = (emailError == undefined || emailError.length == 0) ? undefined : emailError;
+    nomeError = (nomeError == undefined || nomeError.length == 0) ? undefined : nomeError;
+    pontosError = (pontosError == undefined || pontosError.length == 0) ? undefined : pontosError;
+    email = (email == undefined || email.length == 0) ? "" : email;
+
+    // Renderizando pagina passando as variaveis
+    res.render("index", { emailError, nomeError, pontosError, email });
 });
 
 // Rota de captura dados do form com validação de campos
@@ -54,6 +73,13 @@ app.post("/form", (req, res) => {
     }
 
     if (emailError != undefined || nomeError != undefined || pontosError != undefined) {
+        // Flash sessions, passar informações de uma rota para outra,  as sessões duram apenas uma requisição
+        req.flash("emailError", emailError);
+        req.flash("nomeError", nomeError);
+        req.flash("pontosError", pontosError);
+
+        req.flash("email", email);
+
         res.redirect("/");
     } else {
         res.send("Form ok!");
